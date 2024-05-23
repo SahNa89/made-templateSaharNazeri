644db76a7f063d225bf9a432f3bbac8322778e58
SahNa89 <148654084+SahNa89@users.noreply.github.com>
2024-05-23 13:36:17 +0200

    Add files via upload

--- project/Project3.py
+++ project/Project3.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 import sqlite3
+#import urllib.request
 
 dataset_url1 = "https://cidportal.jrc.ec.europa.eu/ftp/jrc-opendata/ABCIS/AtmosphericParticles/Ver2021-01-01/DMPS_Particle_Concentration_2021.csv"
 dataset_url2 = "https://cidportal.jrc.ec.europa.eu/ftp/jrc-opendata/ABCIS/AtmosphericParticles/Ver2021-01-01/Equiv_BlackCarbon_AETH_2021.csv"
@@ -8,15 +9,15 @@ Localpath = "../data"
 os.makedirs(Localpath, exist_ok=True,mode= 0o777)
 
 
-def pipeline(urls, db_name, table_names):
-    df1 = pd.read_csv(urls[0])
-    df2 = pd.read_csv(urls[1])
-    db_path = os.path.join(Localpath, db_name)
-    os.makedirs(os.path.dirname(db_path), exist_ok=True)
-    conn = sqlite3.connect(db_path)
+def CreateData(dataset_urls, table_names):
+    #s = urllib.request.get(dataset_urls).content.decode("utf8")
+    df1 = pd.read_csv(dataset_urls[0])
+    df2 = pd.read_csv(dataset_urls[1])
+    os.makedirs(os.path.dirname("../data/Atmospheric.sqlite"), exist_ok=True)
+    conn = sqlite3.connect("../data/Atmospheric.sqlite")
     df1.to_sql(table_names[0], conn, if_exists="replace", index=False)
     df2.to_sql(table_names[1], conn, if_exists="replace", index=False)
 
     conn.close()
 
-    pipeline([dataset_url1,dataset_url2], "Atmospheric.sqlite", ["DMPS", "BlackCarbon"])
+CreateData([dataset_url1,dataset_url2], ["DMPS", "BlackCarbon"])
